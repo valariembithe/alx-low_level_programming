2be4e2bb9f7c7dcd578a3090d658ac428d13f936
valariembihe <valmbithe691@gmail.com>
2023-03-30 11:58:44 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,18 +2,34 @@
 
 /**
  * cap_string - capitalizes all words
- * @str: string
+ * @s: string
  *
  * Return: string
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i;
+	int count;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] >= 'a' && str [i] <= 'z')
-			str[i] = str[i] - 32;
+/*  scan through string */
+	count = 0;
+	while (s[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
 	}
-	return (str);
+	return (s);
 }

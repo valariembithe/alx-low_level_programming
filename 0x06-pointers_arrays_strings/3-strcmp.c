344e74b13c4cfa5b250e24f666016edf61c3e2ec
valariembihe <valmbithe691@gmail.com>
2023-03-30 11:34:27 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, val;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
 			if (s1[i] < s2[i])
-				return (s1[i] - s2[i]);
+				return (val = s1[i] - s2[i]);
 			else
-				return (s1[i] - s2[i]);
+				return (val = s1[i] - s2[i]);
 		}
 		else
-			return (0);
+			return (val);
 	}
 	return (0);
 }

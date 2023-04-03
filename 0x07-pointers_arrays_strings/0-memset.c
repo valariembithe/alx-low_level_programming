166e5a0ea5e0ce6408d16505e3b636b794d75daa
valariembihe <valmbithe691@gmail.com>
2023-04-03 03:42:53 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset: fills memory with constant byte
+ * @b: constant byte
+ * @s: pointed memory area
+ * @n: bytes of memory to be changed
+ *
+ * Return: 
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}

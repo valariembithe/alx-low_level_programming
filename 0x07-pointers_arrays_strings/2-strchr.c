2e96b83bcc9321cedcec3e459e6d11175ddf7568
valariembihe <valmbithe691@gmail.com>
2023-04-03 04:35:54 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: a character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s);
+	}
+	return(0);
+	
+}

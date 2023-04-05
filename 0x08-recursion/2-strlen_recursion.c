89afd411ead4296116d060668c1bcad03fe896b9
valariembihe <valmbithe691@gmail.com>
2023-04-05 04:48:49 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - string length
+ * @s: string
+ *
+ *Return: length in int
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
+}

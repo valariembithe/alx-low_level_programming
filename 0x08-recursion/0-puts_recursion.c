2a32e8c53bafae7f49ddc35286968e51c6c5b6ea
valariembihe <valmbithe691@gmail.com>
2023-04-05 04:15:16 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print string
+ * @s: string
+ * Return: string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
+

5f5fdc552f4da0d35a125580bc522e20348ff974
valariembihe <valmbithe691@gmail.com>
2023-04-08 05:13:24 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * #argv: argument vector
+ *
+ * Return: 0 success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}

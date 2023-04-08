ef57468f7be39ad5f39129831081c0a2b628f63e
valariembihe <valmbithe691@gmail.com>
2023-04-08 05:02:00 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -8,10 +8,9 @@
  *
  * Return: 0 success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }

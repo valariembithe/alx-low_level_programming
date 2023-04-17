1b3316c49e7f6a7a22cb7ac7952c9360c788d9e6
valariembihe <valmbithe691@gmail.com>
2023-04-17 12:14:29 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - function
+ * @d: pointer
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}

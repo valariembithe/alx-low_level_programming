fb8024c62e95ed018bf25ad5530295603fb970d5
valariembithe <muemavalarie@gmail.com>
2023-05-08 04:29:30 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 	}
-	fd = open(filename, O_WRONLY|O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	w = write(fd, text_content, len);
 	if (fd == -1 || w == -1)
 		return (-1);

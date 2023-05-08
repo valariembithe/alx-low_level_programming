279ca43b4e8720ccd2b0742d30c166e91fa56a4d
valariembithe <muemavalarie@gmail.com>
2023-05-08 04:28:06 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
  * @filename: name of filename to be created
  * @text_content:  is a NULL terminated string to write to the file
  *
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 	}
-	fd = open(filename, O_CREAT|O_RDWR|O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
